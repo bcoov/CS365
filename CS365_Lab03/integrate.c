e82437b47a0255ded75a4889a0318b6c3f6c4f24
Ben Coover <bcoover@ycp.edu>
2016-06-08 13:50:24 -0400

    Lab 03 *.c file

--- CS365_Lab03/integrate.c
+++ CS365_Lab03/integrate.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	double xmin, xmax;
+	double xmin, xmax, total;
 	int nrects;
 
 	if (rank == 0) {
@@ -28,30 +28,43 @@ int main(int argc, char **argv)
 	}
 
 	// Broadcast input values
-	if (rank == 0) {
-		MPI_Bcast(&xmin, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&xmax, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Bcast(nrects, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	} else {
-		MPI_Recv(&xmin, 1, MPI_DOUBLE, 0, MPI_ANY_TAG,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		MPI_Recv(&xmax, 1, MPI_DOUBLE, 0, MPI_ANY_TAG,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		MPI_Recv(&nrects, 1, MPI_DOUBLE, 0, MPI_ANY_TAG,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	}
+	MPI_Bcast(&xmin, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&xmax, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&nrects, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	// Divide up work
-	// TODO
+	int chunk_size = nrects / size;
+	int extra = nrects % size;
+	double step = (xmax - xmin) / (double) nrects;
+	int start = chunk_size * rank;
+	int end = chunk_size * (rank + 1);
+	if (rank == (size - 1)) {
+		end += extra;
+	}
+	//printf("Step width: %f\n", step);
+	//printf("Rank %d: Start at %d, end at %d\n", rank, start, end);
+	//fflush(stdout);
 
 	// Local computation
-	// TODO
+	double x = xmin + (start * step) + (step / 2.0);
+	double y = 0.00;
+	for (int i = start; i < end; i++, x += step) {
+		// y = sin(x) + x^1.3 cos(x) + x ln(x)
+		if (x != 0) {
+			y += (sin(x) + (pow(x, 1.3) * cos(x)) + (x * log(x))) * step;
+		}
+	}
 
+	//printf("Rank %d's y: %f\n", rank, y);
+	//fflush(stdout);
 	// Reduce local partial results to produce one global result
-	// TODO
+	MPI_Reduce(&y, &total, 1, MPI_DOUBLE,
+               MPI_SUM, 0, MPI_COMM_WORLD);
 
 	// Print the global result (process 0 only)
-	// TODO
+	if (rank == 0) {
+		printf("Global total: %f\n", total);
+	}
 
 	MPI_Finalize();
 
