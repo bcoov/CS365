d80bd0ded4832c1063b3f28b7bcd331f423202e5
Ben Coover <benjamin.coover@jci.com>
2016-07-08 10:03:16 -0400

    First attempt at Lab6

--- CS365_Lab06/sumu16_par.c
+++ CS365_Lab06/sumu16_par.c
@@ -9,12 +9,35 @@
 #include <pthread.h>
 #include "common.h"
 
+struct t_parts {
+	uint16_t * t_data;
+	size_t start;
+	size_t end;
+	uint64_t t_sum;
+};
+
+void * thread_sum(void * t_arg)
+{
+	printf("Starting thread_sum\n");
+
+	struct t_parts * thread_data = (struct t_parts *) t_arg;
+	for (size_t i = thread_data->start; i < thread_data->end; ++i) {
+		thread_data->t_sum += thread_data->t_data[i];
+	}
+
+	pthread_exit(NULL);
+}
+
 int main(int argc, char **argv)
 {
 	uint16_t *data;
 	size_t i, size, num_elements;
 	uint64_t sum;
 	struct mmap_region region;
+
+	struct t_parts t0_data;
+	pthread_t thread0;
+	size_t chunk;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: sumu16 <input file>\n");
@@ -28,6 +51,7 @@ int main(int argc, char **argv)
 	// Each element (datum) is 16 bits.
 	// Find out how many elements there are overall.
 	num_elements = size / sizeof(uint16_t);
+	chunk = num_elements / 2;
 
 	// Map the file data into memory
 	map_file_region(argv[1], size, &region);
@@ -35,9 +59,22 @@ int main(int argc, char **argv)
 
 	// Compute the sum
 	sum = 0ULL;
-	for (i = 0; i < num_elements; i++) {
+
+	t0_data.t_data = data;
+	t0_data.start = chunk + (size_t) 1;
+	t0_data.end = t0_data.start + chunk;
+	t0_data.t_sum = sum;
+
+	pthread_create(&thread0, NULL, thread_sum, (void *) t0_data);
+
+	// for (i = 0; i < num_elements; i++) {
+	for (i = 0; i < chunk; i++) {
 		sum += data[i];
 	}
+
+	pthread_join(&thread0, NULL);
+
+	sum += t0_data.t_sum;
 
 	printf("Sum is %lu\n", sum);
 
