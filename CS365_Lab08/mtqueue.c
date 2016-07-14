0cc844ec6936fa9fd3139c081cd3caa9bd714f0a
Ben Coover <bcoover@ycp.edu>
2016-07-14 14:17:48 -0400

    Fix for Assign02 and Lab08 working

--- CS365_Lab08/mtqueue.c
+++ CS365_Lab08/mtqueue.c
@@ -7,7 +7,7 @@ MTQueue *mtqueue_create(void)
 	MTQueue *q = malloc(sizeof(MTQueue));
 
 	q->head = q->tail = NULL;
-    q->head->next = q->tail;
+    //q->head->next = q->tail;
 
 	pthread_mutex_init(&q->lock, NULL);
     pthread_cond_init(&q->cond, NULL);
@@ -23,8 +23,14 @@ void mtqueue_enqueue(MTQueue *q, void *item)
 
     pthread_mutex_lock(&q->lock);
 
-    q->tail->next = new_node;
-    q->tail = new_node;
+	if (q->head == NULL) {
+		q->head = new_node;
+		q->tail = new_node;
+	}
+	else {
+	    q->tail->next = new_node;
+    	q->tail = new_node;
+	}
     pthread_cond_broadcast(&q->cond);
 
     pthread_mutex_unlock(&q->lock);
@@ -32,17 +38,31 @@ void mtqueue_enqueue(MTQueue *q, void *item)
 
 void *mtqueue_dequeue(MTQueue *q)
 {
-	mtqueue_wait_until_empty(q);
+	//mtqueue_wait_until_empty(q);
 
     pthread_mutex_lock(&q->lock);
 
+	// wait for queue to become non-empty
+	while (q->head == NULL) {
+		pthread_cond_wait(&q->cond, &q->lock);
+	}
+
     MTQueueNode * to_remove = q->head;
     q->head = q->head->next;
-    free(q->head);
+    //free(q->head);
+
+	// if head became null, make tail null as well
+	if (q->head == NULL) {
+		q->tail = NULL;
+	}
+	pthread_cond_broadcast(&q->cond);
 
     pthread_mutex_unlock(&q->lock);
 
-    return to_remove->item;
+    //return to_remove->item;
+	void *item = to_remove->item;
+	free(to_remove);
+	return item;
 }
 
 void mtqueue_wait_until_empty(MTQueue *q)
