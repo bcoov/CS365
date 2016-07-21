3332e1ba4474d5c5ba2f32e91a1f9564ee8e6bdf
Ben Coover <bcoover@ycp.edu>
2016-07-21 14:43:21 -0400

    Remove debug printout

--- CS365_Exam01/countblocks.c
+++ CS365_Exam01/countblocks.c
@@ -87,8 +87,8 @@ int main(int argc, char **argv)
 	int pr_end = pr_start + p_rows;
 	int pc_end = pc_start + p_cols;
 
-	printf("Start: %d, End: %d; (Rank: %d)\n", pr_start, pr_end, rank);
-	printf("Start: %d, End: %d; (Rank: %d)\n", pc_start, pc_end, rank);
+	//printf("Start: %d, End: %d; (Rank: %d)\n", pr_start, pr_end, rank);
+	//printf("Start: %d, End: %d; (Rank: %d)\n", pc_start, pc_end, rank);
 
 	// TODO: local computation: count how many 3x3 blocks in this
 	// process's region sum to q
@@ -109,13 +109,13 @@ int main(int argc, char **argv)
 			uint8_t total = (UL + UM + UR +
 							 ML + MM + MR +
 							 BL + BM + BR);
-			printf("%d %d %d %d %d %d %d %d %d => %d\n", UL, UM, UR, ML, MM, MR, BL, BM, BR, total);
+			//printf("%d %d %d %d %d %d %d %d %d => %d\n", UL, UM, UR, ML, MM, MR, BL, BM, BR, total);
 			if (total == q) {
 				p_count++;
 			}
 		}
 	}
-	printf("count for rank %d: %d\n", rank, p_count);
+	//printf("count for rank %d: %d\n", rank, p_count);
 
 	// TODO: reduce all of the local results to a single
 	// overall result, report it (hint: MPI_Reduce)
